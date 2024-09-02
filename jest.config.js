feb4ebc712d52efe8f9082742d2a18925942010f
chaeney <chaekey@gmail.com>
2024-09-02 18:38:44 +0900

    render test

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"], // 올바른 설정
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
--- src/pages/home.test.tsx
+++ src/pages/home.test.tsx
@@ -1,9 +1,13 @@
-import { render } from "@testing-library/react";
-import "@testing-library/dom";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Home from "./home";
+import React from "react";
 
 describe("Page", () => {
   it("renders the page", () => {
     render(<Home />);
+    expect(screen.getByText("Get started by editing")).toHaveTextContent(
+      "Get started by editing"
+    );
   });
 });
